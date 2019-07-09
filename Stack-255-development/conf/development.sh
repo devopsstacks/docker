f495507a10d2c6d7e5c9b7cee00bcb82cd88686b
alvarofloresPM <alvaro.flores@processmaker.com>
2019-07-09 15:00:27 -0400

    development tools

--- Stack-255-development/conf/development.sh
+++ Stack-255-development/conf/development.sh
@@ -5,13 +5,15 @@ yum install -y which ;
 yum install -y unzip ;
 yum groupinstall "Development tools" -y ;
 curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer && chmod +x /usr/local/bin/composer ;
-yum install -y gcc-c++ patch readline readline-devel zlib zlib-devel \ 
-libyaml-devel libffi-devel openssl-devel make \
-bzip2 autoconf automake libtool bison iconv-devel ;
-sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 ;
-curl -sSL https://get.rvm.io | sudo bash -s stable ;
+yum install -y gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison iconv-devel ;
+curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
+curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 ;
+curl -sSL https://get.rvm.io | bash -s stable ;
+usermod -a -G rvm `whoami` ;
+sleep 60 ;
 source /usr/local/rvm/bin/rvm ;
-rvm reload  ;
+rvm reload ;
 rvm requirements run ;
 rvm install 2.3 ;
 rvm use 2.3 --defau ;
