77d606bc700f6721af8476798981f460d870c1a7
alvarofloresPM <alvaro.flores@processmaker.com>
2021-09-09 14:38:31 -0400

    changes KR-11062

--- Stack-N275/conf/servicespm.sh
+++ Stack-N275/conf/servicespm.sh
@@ -14,14 +14,14 @@ fi
 echo "
        ░░░░░░░
     ░░░░░░░░░░░░░
-   ░░░░       ░░░░     WELCOME TO PROCESSMAKER STACK N275 -> ( amazonlinux:2017.09 ; NGINX-1.12 ; PHP-7.3 )
+   ░░░░       ░░░░     WELCOME TO PROCESSMAKER STACK N275 -> ( amazonlinux:2017.09 ; NGINX-1.12 ; PHP-7.4 )
   ░░░░  ░░░░░   ░░░
   ░░░  ░░░░░░░  ░░░░   - This ProcessMaker Stack uses MySql 8
   ░░░  ░░░░░░   ░░░    - The following command runs mysql8 in Docker:
-   ░░  ░░     ░░░░     -> docker run --name pm-db8 -e MYSQL_ROOT_PASSWORD=PM-Testdb -p 3306:3306 -d mysql:8 mysqld --default-authentication-plugin=mysql_native_password
+   ░░  ░░     ░░░░     -> docker run --name pm-db8 -e MYSQL_ROOT_PASSWORD=PM-Testdb -p 3306:3306 -d mysql:8 mysqld --default-authentication-plugin='mysql_native_password' --optimizer-switch='derived_merge=off' --sql-mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'
     ░  ░░░░░░░░░       
        ░░░░░░░         For more information see https://www.processmaker.com
-                                                http://wiki.processmaker.com/3.2/Supported_Stacks
+                                                http://wiki.processmaker.com/3.5/Supported_Stacks
 	 " ; 
 sleep 3 ;
 pkill php-fpm ;
